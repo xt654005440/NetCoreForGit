d8593db230b5da230cbb5d98d996c00e085f1e3a
Xutao <Xutao@DESKTOP-VKKC5KB>
2021-05-30 18:03:24 +0800

    C# 1

--- CommonClass/Name.cs
+++ CommonClass/Name.cs
@@ -2,8 +2,127 @@
 
 namespace CommonClass
 {
+    public class Person
+    {
+        #region 构造函数
+        public Person()
+        {
+
+        }
+        #endregion
+
+        #region 析构函数
+        ~Person()
+        {
+        }
+        #endregion
+
+        #region 属性
+        private string _name;
+        private int _age;
+        private string _identity;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public string Identity
+        {
+            get { return _identity; }
+            set { _identity = value; }
+        }
+
+        public string Id { get; } = Guid.NewGuid().ToString();
+        #endregion
+
+        public void PrintName(string name, string xing)
+        {
+
+            Console.WriteLine(String.Format("{0}-{1}", name, xing));
+
+        }
+
+        /// <summary>
+        /// 可变参数方法
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public int Count(params int[] data)
+        {
+
+            int m = 0;
+            foreach (int item in data)
+            {
+                m += item;
+            }
+            return m;
+
+        }
+
+    }
     public class Name
     {
 
     }
+    public class NamespaceExample
+    {
+        public string GetNamespace()
+        {
+            return this.GetType().Namespace;
+        }
+    }
+
+    //实现单例模式
+    public class SingLeton
+    {
+        private static SingLeton _singLeton; //声明静态类型字段
+        private int _state;
+        private SingLeton(int state)         //声明私有构造函数，即只能在类内部调用构造函数
+        {
+            _state = state;
+        }
+
+        public static SingLeton Instance   //通过静态属性实现创建唯一的类的实例，实现单例
+        {
+            get => _singLeton ?? (_singLeton = new SingLeton(44));   //表达式形式get方法
+        }
+    }
+
+    //静态构造函数
+    public static class UserPreference
+    {
+        public static Color BackColor { get; }
+
+        static UserPreference()//静态构造函数
+        {
+            DateTime now = DateTime.Now;
+            if (now.DayOfWeek == DayOfWeek.Saturday || now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                BackColor = Color.Green;
+            }
+            else
+            {
+                BackColor = Color.Red;
+            }
+
+        }
+    }
+
+    public enum Color
+    {
+        White,
+        Red,
+        Green,
+        Blue,
+        Black
+    }
+
+
+
 }
--- text1/text1/Program.cs
+++ text1/text1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using CommonClass;
+using Introduction = CommonClass;
 
 namespace text1
 {
@@ -7,6 +9,132 @@ namespace text1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            #region C#基础
+            //    int a = 1;
+            //    int b = 2;
+            //    bool isTrue = false;
+            //    Console.WriteLine($"isTrue is {isTrue.ToString()}");
+            //    Console.WriteLine($"Now isTrue is {isTrue.ToString()}");
+
+            //    //if语句
+            //    if (a > b)
+            //    {
+            //        isTrue = false;
+            //    }
+            //    else
+            //    {
+            //        isTrue = true;
+            //    }
+
+            //    //switch语句
+            //    int x = 1;
+            //    int y = 0;
+            //    switch (x)
+            //    {
+            //        case 1:
+            //            y = 1;
+            //            break;
+            //        case 2:
+            //        case 3:
+            //            y = 2;
+            //            break;
+            //        //case x > 3:
+            //        //    break;
+            //        default:
+            //            break;
+            //    }
+
+            //    //for循环
+            //    string myName = "Forest";
+            //    int rVale = 0;
+            //    for (int i = 0; i < myName.Length; i++)
+            //    {
+            //        rVale = i + rVale;
+            //    }
+            //    Console.WriteLine($"Now,rVale is {rVale}");
+
+
+            //    //嵌套循环输出二维矩阵
+            //    int gotoFlag = 0;
+            //forLable:
+            //    for (int m = 0; m < 100; m += 10)
+            //    {
+            //        for (int n = m; n < m + 10; n++)
+            //        {
+            //            Console.Write($"{n} ");
+            //        }
+            //        Console.WriteLine();
+            //    }
+
+            //    //while循环
+            //    int flagwhile = 1;
+            //    while (flagwhile > 0)
+            //    {
+            //        Console.WriteLine("This is a while.");
+            //        break;
+            //    }
+
+            //    ////foreach 循环便利集合对象
+            //    //foreach (var item in collection)
+            //    //{
+
+            //    //}
+
+            //    //goto 跳转 再次输出二维矩阵 *通常情况下不允许使用goto 这不是面向对象的良好设计
+            //    if (gotoFlag < 3)
+            //    {
+            //        gotoFlag++;
+            //        goto forLable;
+            //    }
+            //    gotoFlag = 0;
+
+            //    string eg = "\\//";
+
+            //    Console.WriteLine(eg);
+
+            #endregion
+
+            Introduction::NamespaceExample nameItem = new Introduction.NamespaceExample();  // :: 名称空间修饰符 
+
+            //Person p = new Person();
+            //Console.WriteLine(p);
+            //Console.WriteLine(p.Id);
+
+
+            ////匿名类型
+            //var caption = new
+            //{
+            //    FirstName = "James",
+            //    MiddleName = "T",
+            //    LastName = "Kirk",
+            //};
+            //Console.WriteLine(caption.FirstName);
+            //Console.WriteLine(caption.MiddleName);
+            //Console.WriteLine(caption.LastName);
+            //Console.WriteLine(String.Format("{0}-{1}-{2}",
+            //    caption.FirstName,
+            //    caption.MiddleName,
+            //    caption.LastName)
+            //    );
+
+
+            ////方法
+            //Person pp = new Person();
+            //pp.PrintName("Tao", "Xu");
+            //pp.PrintName(name: "Tao", xing: "Xu"); // 命名参数；
+
+            ////个数可变参数方法,Count方法使用params关键字定义参数  类型为整型数组，即可以传递一个整型数组
+            //Console.WriteLine(pp.Count(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).ToString());
+
+            //静态构造函数
+            string colorStr = UserPreference.BackColor.ToString();
+            int colorIntger = (int)UserPreference.BackColor;
+
+            Console.WriteLine($"colorStr is {colorStr}");
+            Console.WriteLine($"colorIntger is {colorIntger.ToString()}");
+
+            Console.ReadKey();
         }
     }
 }
